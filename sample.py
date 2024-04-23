d76b73d1b661616ee9bc8c65d6a9c2277ac54f34
raziqpv333 <raziqpv333@gmail.com>
2024-04-23 13:46:59 +0530

    pythonworkstarted

---
+++ sample.py
@@ -0,0 +1,18 @@
+ print("WELCOME")
+print("name is king")
+print("age is 27")
+print("course is python")
+age=24
+name="anu"
+print(age)
+print(name)
+stdage=20
+print(stdage)
+stdage=20
+print(stdage)
+std_name="anuu"
+print(std_name)
+a2=20
+print(a2)
+a=30
+print(a)
