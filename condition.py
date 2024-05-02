c6660615211bb30c870b190cc240c5f0369d9d6f
raziqpv333 <raziqpv333@gmail.com>
2024-05-02 13:28:27 +0530

    gittey

---
+++ condition.py
@@ -0,0 +1,33 @@
+a=10
+b=10
+if a==b:
+    print('eql')
+else :
+    print('not')   
+z=int(input("enter your age"))
+s=int(input("enter your age"))
+print("your age is",z)
+print("your age is",s)
+if z==s:
+    print('eql')
+else:
+    print('not')    
+
+
+d=int(input("enter your age"))
+
+if d>=18:
+    print('eligble for vote')
+else:
+    print('not eligible for vote')
+w=int(input("enter your number"))
+f=int(input("enter your number"))
+if w>f:
+    print("largest number",w)
+else:
+    print('largest number',f)
+mark=int(input("enter mark"))
+if mark>30:
+    print('passed')
+else:
+    print('failed')
