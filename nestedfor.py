f0ebef56911cd949dad7d791bc47a9a54151f4b3
raziqpv333 <raziqpv333@gmail.com>
2024-05-08 13:29:02 +0530

    tasks

---
+++ nestedfor.py
@@ -0,0 +1,66 @@
+for i in range(3):
+    for s in range(4):
+        print(s)
+
+for i in range(6):
+    for f in range(3):
+        print("*")
+    print()
+
+for i in range(3):
+    for h in range(4):
+        print(h,end=" ")
+    print()        
+
+for i in range(3):
+    for v in range(5):
+        print("*",end=" ")
+    print()
+
+for i in range(3):
+    for c in range(5,9):
+        print(c,end=" ")
+    print()
+
+for i in range(1,3):
+    for s in range(3):
+        print(i,end=" ")
+    print()  
+
+for i in range(3):
+    for k in range(1,6,2):
+        print(k,end=" ")
+    print()  
+
+for i in range(3):
+    for q in range(0,5,2):
+        print(q,end=" ")
+    print()   
+
+a=1
+for i in range(3):
+    for j in range(3):
+       print(a,end=" ")
+       a+=1
+    print()
+
+
+for i in range(3):
+    x=i
+    for d in range(3):
+
+         
+        print(x,end=" ") 
+        x+=1
+    print()
+
+for i in range(3):
+    e=i
+    for s in range(3):
+        print(e,end=" ")
+        e-=9
+    print()    
+
+
+    
+          
