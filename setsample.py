3a0819c401fc5fea782695ff72ecd167ae9e23bd
raziqpv333 <raziqpv333@gmail.com>
2024-05-18 11:45:32 +0530

    tupset

---
+++ setsample.py
@@ -0,0 +1,15 @@
+t={100}
+print(type(t))
+t={1,2,3}
+print(t)
+t={10,20,30}
+for i in t:
+    print(i)
+
+
+j={10,20,30,22}
+
+if 20 in j:
+        print("yes") 
+else:
+        print('no') 
---
+++ tuplesample.py
@@ -0,0 +1,18 @@
+t=(100,)
+print(type(t))
+t=(1,2,3)
+print(t)
+t=(1,1,2,4,5,1,2)  
+print(t.count(1))
+f=(10,20,30,40)
+print(f.index(20))
+t=(10,20,30)
+for i in t:
+    print(i)
+
+j=(10,20,30,22)
+
+if 20 in j:
+        print("yes") 
+else:
+        print('no') 
