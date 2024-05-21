82b2ac5ee5a94f18962d022e5ce287d5140ed97a
raziqpv333 <raziqpv333@gmail.com>
2024-05-21 09:17:52 +0530

    setfiles

---
+++ diction.py
@@ -0,0 +1,26 @@
+d={}
+m={'name':"asi",'age':24,'place':"ekm"}
+print(m)
+
+f={'name':'amee','age':23,'place':'calicut'}
+for i in f:
+        print(i)
+
+d1={'name':'roshan','age':23,'place':'alappy'}    
+for i in d1:
+        print(d1[i])    
+
+d2={'name':'sony','age':24,'place':'idukki'}
+print(d2['name'],d2['age'],d2['place'])
+       
+p={'name':'moly','age':54,'place':'kotayam'}     
+print(p['name'],p['age'],p['place'])
+p['age']=50
+print(p['name'],p['age'],p['place'])
+
+j={'name':'joy','age':54,'place':'trissur'} 
+print(j['name'],j['age'],j['place'])
+j['age1']=56
+print(j['name'],j['age'],j['place'],j['age1'])   
+
+  
